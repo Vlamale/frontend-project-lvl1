b7674ee72dd05958405cb2ab99e05576505fbd38
Vlad <kristoyt@bk.ru>
2022-02-01 12:26:37 +0300

    fix prime function

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -2,11 +2,10 @@ import { engine, generateRandomNumber } from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const getRightAnswer = (num) => {
+  if (num <= 1) return 'no';
   /* eslint-disable no-plusplus */
   for (let i = 2; i <= num / 2; i++) {
-    if (num % i === 0) {
-      return 'no';
-    }
+    if (num % i === 0) return 'no';
   }
   /* eslint-enable no-plusplus */
   return 'yes';
